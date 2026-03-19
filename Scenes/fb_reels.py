4d7cc87c704eda6e537bb74822f7512680eacc91
McBrown Mwale <mcbrownwilfred@gmail.com>
2026-03-19 14:47:35 +0200

    Update fb_reels.py

--- Scenes/fb_reels.py
+++ Scenes/fb_reels.py
@@ -4,6 +4,7 @@ config.frame_width = 9
 config.frame_height = 16
 config.frame_rate = 30
 config.disable_caching = True
+# config.background_color = GRAY_E
 
 
 class Reel(Scene):
@@ -35,24 +36,70 @@ class Reel(Scene):
             MathTex(r"\sqrt[3]{k^3} = \sqrt[3]{\frac{b}{x + 1}}"),
             MathTex(r"k = \sqrt[3]{\frac{b}{x + 1}}"),
         ).arrange(DOWN, buff=0.5)
+        rectangle_box = SurroundingRectangle(
+            eq_group[8], buff=0.2, color=PURE_RED, corner_radius=0.2
+        )
+        cancel_1 = MathTex(
+            r"k^3 \times x = \frac{b - k^3}{\cancel{k^3}} \times \cancel{k^3}",
+            tex_template=my_template,
+        )
+        cancel_1.move_to(eq_group[0])
+        cancel_2 = MathTex(
+            r"\frac{k^3\cancel{(x + 1)}}{\cancel{(x + 1)}} = \frac{b}{x + 1}",
+            tex_template=my_template,
+        )
+        cancel_2.move_to(eq_group[5])
+        cancel_3 = MathTex(
+            r"\sqrt[{\cancel{3}}]{k^{\cancel{3}}} = \sqrt[3]{\frac{b}{x + 1}}",
+            tex_template=my_template,
+        )
+        cancel_3.move_to(eq_group[7])
 
         self.play(Write(problem_group[0]))
         self.wait(2)
-        self.play(problem_group.animate.to_edge(UP).scale(1.1).set_color(YELLOW_B))
+        self.play(problem_group.animate.to_edge(UP).scale(1.5).set_color(YELLOW))
         self.wait(2)
-        self.play(Write(eq_group), run_time=10)
+        self.play(Write(eq_group[0]))
+        self.wait(2)
+        self.play(ReplacementTransform(eq_group[0], cancel_1))
+        self.wait(2)
+        self.play(TransformFromCopy(eq_group[0], eq_group[1]))
+        self.wait(2)
+        self.play(TransformFromCopy(eq_group[1], eq_group[2]))
+        self.wait(2)
+        self.play(TransformFromCopy(eq_group[2], eq_group[3]))
+        self.wait(2)
+        self.play(TransformFromCopy(eq_group[3], eq_group[4]))
+        self.wait(2)
+        self.play(TransformFromCopy(eq_group[4], eq_group[5]))
+        self.wait(2)
+        self.play(ReplacementTransform(eq_group[5], cancel_2))
+        self.wait(2)
+        self.play(TransformFromCopy(eq_group[5], eq_group[6]))
+        self.wait(2)
+        self.play(TransformFromCopy(eq_group[6], eq_group[7]))
+        self.wait(2)
+        self.play(ReplacementTransform(eq_group[7], cancel_3))
+        self.wait(2)
+        self.play(TransformFromCopy(eq_group[7], eq_group[8]))
+        self.wait(2)
+        self.play(Create(rectangle_box))
         self.wait(2)
 
         # Outro
-        final_text = Tex("Thank you for watching!", color=YELLOW_B)
+        final_text = Tex("Thank you for watching!", color=YELLOW)
         self.play(
             Write(final_text),
-            ShrinkToCenter(VGroup(problem_group, eq_group)),
+            ShrinkToCenter(
+                VGroup(
+                    problem_group, eq_group, rectangle_box, cancel_1, cancel_2, cancel_3
+                )
+            ),
         )
         self.wait()
         self.play(
             logo_corner.animate.move_to(ORIGIN).scale(3),
-            final_text.animate.to_edge(DOWN).set_color(WHITE).scale(1.3),
+            final_text.animate.shift(DOWN * 4).set_color(WHITE).scale(1.3),
         )
         self.wait()
         self.play(FadeOut(final_text, logo_corner))
